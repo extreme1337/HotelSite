4d7644e21990be32aab70c411d7836e7b9dd8440
Marko Miseljic <marko.miseljic.14@gmail.com>
2021-02-02 10:16:59 +0100

    Initial commit

---
+++ property/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+property_type = (
+    ("S", "sale"),
+    ("R", "rent")
+)
+
+# Create your models here.
+class Property(models.Model):
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=5, choices=property_type)
+    price = models.FloatField()
+    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
+    area = models.FloatField()
+    beds_number = models.PositiveIntegerField()
+    baths_number = models.PositiveIntegerField()
+    garages_number = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='property/', null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Property'
+        verbose_name_plural = 'Properties'
+    
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
